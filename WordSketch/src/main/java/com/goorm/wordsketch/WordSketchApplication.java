bc6e2006316bf286111edff21d97bbc31ee2f27d
박민혁 <sis03090@naver.com>
2024-03-21 15:52:03 +0900

    🎉 스프링 초기 설정

---
+++ WordSketch/src/main/java/com/goorm/wordsketch/WordSketchApplication.java
@@ -0,0 +1,13 @@
+package com.goorm.wordsketch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordSketchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordSketchApplication.class, args);
+	}
+
+}
