d432501267caa7d8872c6ad9c1909baadbb74908
Inbeom Kim <dlsqja970210@naver.com>
2024-04-17 11:31:29 +0900

    🐛 UserRole 수정

--- src/main/java/com/goorm/wordsketch/entity/UserRole.java
+++ src/main/java/com/goorm/wordsketch/entity/UserRole.java
@@ -10,7 +10,7 @@ public enum UserRole {
     * 스프링 시큐리티에서는 권한(Role) 코드에 항상
     * "ROLE_" 접두사가 앞에 붙어야 하기 떄문에 "ROLE_"을 설정
     * */
-    GEUST("ROLE_GUEST"), USER("ROLE_USER"), ADMIN("ROLE_ADMIN");
+    GUEST("ROLE_GUEST"), USER("ROLE_USER"), ADMIN("ROLE_ADMIN");
 
     private final String key;
 }
