d2729d0ece696d7046ecaa5db19ee48c9444832b
Inbeom Kim <dlsqja970210@naver.com>
2024-04-19 13:53:36 +0900

    🔊 시큐리티 loggin level 수정 및 로그 추가

--- src/main/java/com/goorm/wordsketch/util/JwtTokenValidatorFilter.java
+++ src/main/java/com/goorm/wordsketch/util/JwtTokenValidatorFilter.java
@@ -26,6 +26,8 @@ public class JwtTokenValidatorFilter extends OncePerRequestFilter {
      * */
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // TODO: 로그 출력
+        log.info("path = " + request.getServletPath());
         jwtService.validateAccessToken(request, response);
 
         filterChain.doFilter(request, response);
